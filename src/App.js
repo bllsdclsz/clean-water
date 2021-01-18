47e25d57d9aafa78c28929d4321c44e282360715
ekolog <ekolog055@gmail.com>
2021-01-18 23:13:40 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Route, Switch } from 'react-router-dom';
 import Homepage from "./components/Homepage.js";
 import Aboutuspage from "./components/Aboutuspage.js";
 import Projectspage from "./components/Projectspage.js";
@@ -11,14 +11,16 @@ import "./components/images/waterfall.jpg";
 function App() {
   return (
     <HashRouter basename={process.env.PUBLIC_URL}>
-      <Navigation />
-      <Switch>
-      <Route path="/" exact component={Homepage}/>
-      <Route path="/aboutus" exact component={Aboutuspage}/>
-      <Route path="/projects" exact component={Projectspage}/>
-      <Route path="/contact" exact component={Contactpage}/>
-      </Switch>
-      <Footer />
+      <Router>
+        <Navigation />
+          <Switch>
+          <Route path="/" exact component={Homepage}/>
+          <Route path="/aboutus" exact component={Aboutuspage}/>
+          <Route path="/projects" exact component={Projectspage}/>
+          <Route path="/contact" exact component={Contactpage}/>
+        </Switch>
+        <Footer />
+      </Router>
     </HashRouter>
   );
 }
