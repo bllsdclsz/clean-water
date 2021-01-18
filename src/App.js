02cd5740c9b6bd370eb5b4b5fbaf07afacc8bc09
ekolog <ekolog055@gmail.com>
2021-01-18 22:38:14 +0100

    hashrouter added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Homepage from "./components/Homepage.js";
 import Aboutuspage from "./components/Aboutuspage.js";
 import Projectspage from "./components/Projectspage.js";
@@ -10,14 +10,16 @@ import "./components/images/waterfall.jpg";
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Navigation />
+      <Switch>
       <Route path="/" exact component={Homepage}/>
       <Route path="/aboutus/" component={Aboutuspage}/>
       <Route path="/projects/" component={Projectspage}/>
       <Route path="/contact/" component={Contactpage}/>
+      </Switch>
       <Footer />
-    </Router>
+    </HashRouter>
   );
 }
 
