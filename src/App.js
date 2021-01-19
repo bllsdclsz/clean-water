a1595412cbac5310abde25232260edded81dec19
ekolog <ekolog055@gmail.com>
2021-01-19 16:41:47 +0100

    router

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./components/images/waterfall.jpg";
 
 function App() {
   return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Navigation />
           <Switch>
           <Route path="/" exact component={Homepage}/>
